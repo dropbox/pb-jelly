243138b3c5babc866a65f7e6d0c25cf02e215c61
Nipunn Koorapati <nipunn@dropbox.com>
2020-07-13 18:34:54 -0700

    Update imports so gen_protos runs - still seems to fail on imports, but got further

--- gen_protos.sh
+++ gen_protos.sh
@@ -1,1 +1,5 @@
-protoc --plugin=protoc-gen-rust=codegen/codegen.py --proto_path=proto/ --rust_out=generated/ `find proto -name "*.proto"`
+# Install protoc
+# - https://github.com/protocolbuffers/protobuf
+# Install gogoproto
+# - go get github.com/gogo/protobuf/proto
+protoc --plugin=protoc-gen-rust=codegen/codegen.py --proto_path=proto/ --proto_path=$GOPATH/src --rust_out=generated/ `find proto -name "*.proto"`
