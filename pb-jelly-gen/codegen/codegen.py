6d873ab59102a347c78799ed3bd59ee61a77a7bf
Graham King <graham@gkgk.org>
2021-10-19 21:04:27 -0700

    [codegen] Fix two clippy lints in generated code
    - `mem::replace` with Vec -> `mem::take` (because Vec implements
       Default).
    - `as_ref()` with `|ref s|` -> remove the `ref` (reference to a
      reference).

--- pb-jelly-gen/codegen/codegen.py
+++ pb-jelly-gen/codegen/codegen.py
@@ -417,7 +417,7 @@ class RustType(object):
         elif self.field.type == FieldDescriptorProto.TYPE_STRING:
             return (
                 "&str",
-                'self.%s.as_ref().map(|ref s| s.as_str()).unwrap_or("")' % name,
+                'self.%s.as_ref().map(|s| s.as_str()).unwrap_or("")' % name,
             )
         elif self.field.type == FieldDescriptorProto.TYPE_BYTES:
             assert not (
@@ -943,7 +943,7 @@ class CodeWriter(object):
                             % (field.name, typ.rust_type()),
                         ):
                             self.write(
-                                "::std::mem::replace(&mut self.%s, ::std::vec::Vec::new())"
+                                "::std::mem::take(&mut self.%s)"
                                 % field.name
                             )
 
